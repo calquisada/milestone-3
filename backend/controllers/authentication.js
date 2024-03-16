69d34fbc650716ba38a6797c2cc75680b44ef6d9
Cam <farrcameron@gmail.com>
2024-03-16 13:10:54 -0700

    Update authentication.js
    password limit of 20, not sure what is common

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,3 +1,21 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+
+const {User} = db
+// put placeholder while waiting for keywords from group
+router.post('/', async (req,res) => {
+    let {password, ...placeholder} = req.body;
+    const user = await User.create({
+        ...placeholder,
+        passwordDigest: await bcrypt.hash(password, 20)
+    })
+    res.json(user)
+})
+
+router.get('/', async (req, res) => {
+    const users = await User.findAll()
+    res.json(users)
+})
+
+module.exports = router
