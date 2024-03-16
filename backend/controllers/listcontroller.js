1c72c0725c7849dfa970913fc5829dda1eec9726
Cam <farrcameron@gmail.com>
2024-03-16 09:52:20 -0700

    Deletion and syntax fixes on comments

--- backend/controllers/listcontroller.js
+++ backend/controllers/listcontroller.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const router = express.Router();
 const Todo = require("../models/");
 
 // Home Page
 Todo.get('/', (req, res) => {
-    db.Todos.find()
+    db.Todos.find() 
         .then(todos => {
             res.render('index', { todos }); 
         })
         .catch(err => {
-            console.error(err); ///Can be turned into res.render('error404') once we get 404 pages done
+            console.error(err); // Can be turned into res.render('error404') once we get 404 pages done
             res.status(500).send('Server Error');
     });
 });
@@ -18,11 +17,16 @@ Todo.get('/', (req, res) => {
 Todo.post('/', (req, res) => {
     db.Todos.create(req.body)
         .then(() => {
-            res.redirect('/') ///fill in
+            res.redirect('/') // Fill in
         })
         .catch(err => {
-            console.error(err); ///Can be turned into res.render('error404') once we get 404 pages done
+            console.error(err); // Can be turned into res.render('error404') once we get 404 pages done
             res.status(500).send('Server Error');
         })
 })
 
+// Deleting a Todo
+Todo.delete('/:id', (req, res) => {})  
+
+// Editing a Todo
+Todo.put('/:id', (req, res) => {})
