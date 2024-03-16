117153b9b873c59c1a7406224915dbf0df701664
Megan Lake <meganlake95@gmail.com>
2024-03-16 10:58:48 -0700

    designed loginpage

--- src/components/loginpage.jsx
+++ src/components/loginpage.jsx
@@ -1,1 +1,22 @@
-// authentication and authorization
+// import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function LoginForm() {
+    return (
+        <Form method='POST'>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label >Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" className='form-box'/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label >Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" className='form-box'/>
+            </Form.Group>
+            <button type="submit">
+                Submit
+            </button>
+        </Form>
+    );
+}
+
+export default LoginForm;
