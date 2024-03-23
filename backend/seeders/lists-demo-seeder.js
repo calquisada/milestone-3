d7c141d738bbe83818930af17265eb5ff4f442ff
Cam <farrcameron@gmail.com>
2024-03-23 11:43:27 -0700

    Fixed lists-demo-seeder.js

--- backend/seeders/lists-demo-seeder.js
+++ backend/seeders/lists-demo-seeder.js
@@ -4,34 +4,31 @@ const { lists } = require('../models');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const list = [
+    const listItems = [
       {
-        task: 'Buy groceries',
-        completed: false,
+        listname: 'Grocery List',
+        listitem1: 'Apples',
+        listitem2: 'Bananas',
+        listitem3: 'Milk',
+        listitem4: 'Bread',
+        listitem5: 'Eggs',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        task: 'Clean the house',
-        completed: false,
+        listname: 'Todo List',
+        listitem1: 'Check the garden',
+        listitem2: 'Feed the dog',
+        listitem3: 'Clean the house',
+        listitem4: 'Take the dog out',
+        listitem5: 'Homework',
         createdAt: new Date(),
         updatedAt: new Date()
       },
-      {
-        task: 'Take dog out',
-        completed: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        task: 'Do homework',
-        completed: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
+      
     ];
 
-    await queryInterface.bulkInsert('lists', list, {});
+    await queryInterface.bulkInsert('lists', listItems, {});
   },
 
   down: async (queryInterface, Sequelize) => {
